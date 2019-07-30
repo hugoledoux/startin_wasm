63d2b3c5a713111d429430a5fd7a535b237c2a9e
Hugo Ledoux <h.ledoux@tudelft.nl>
2019-07-30 16:55:36 +0200

    update the wasm compiled code

--- www/dist/bootstrap.js
+++ www/dist/bootstrap.js
@@ -159,7 +159,7 @@
 /******/ 				promises.push(installedWasmModuleData);
 /******/ 			else {
 /******/ 				var importObject = wasmImportObjects[wasmModuleId]();
-/******/ 				var req = fetch(__webpack_require__.p + "" + {"../pkg/startin_wasm_hledoux_bg.wasm":"f00db4625c015a1c69a2"}[wasmModuleId] + ".module.wasm");
+/******/ 				var req = fetch(__webpack_require__.p + "" + {"../pkg/startin_wasm_hledoux_bg.wasm":"df80ef013dd572954c58"}[wasmModuleId] + ".module.wasm");
 /******/ 				var promise;
 /******/ 				if(importObject instanceof Promise && typeof WebAssembly.compileStreaming === 'function') {
 /******/ 					promise = Promise.all([WebAssembly.compileStreaming(req), importObject]).then(function(items) {
