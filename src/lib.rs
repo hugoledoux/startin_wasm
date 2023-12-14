61b3384ad3549a1394b7d53027934cdbf4183e24
Hugo Ledoux <h.ledoux@tudelft.nl>
2023-12-14 16:51:42 +0100

    Update to startin v0.7.1 + draw Voronoi diagram

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-use startin::Triangulation;
+extern crate startin;
 use wasm_bindgen::prelude::*;
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
@@ -14,13 +14,13 @@ extern "C" {
 
 #[wasm_bindgen]
 pub struct DT {
-    t: Triangulation,
+    t: startin::Triangulation,
 }
 
 #[wasm_bindgen]
 impl DT {
     pub fn new() -> DT {
-        let dt = Triangulation::new();
+        let dt = startin::Triangulation::new();
         DT { t: dt }
     }
 
@@ -48,7 +48,7 @@ impl DT {
     }
 
     pub fn all_edges(&self) -> Vec<usize> {
-        self.t.all_edges()
+        self.t.all_finite_edges()
     }
 
     pub fn all_triangles(&self) -> Vec<usize> {
@@ -62,9 +62,63 @@ impl DT {
         trs
     }
 
+    pub fn all_voronoi_edges(&self) -> Vec<f64> {
+        let mut edges: Vec<f64> = Vec::new();
+        let pts = self.t.all_vertices();
+        let trs = self.t.all_finite_triangles();
+        for each in trs.iter() {
+            let cc =
+                startin::geom::circle_centre(&pts[each.v[0]], &pts[each.v[1]], &pts[each.v[2]]);
+            let adjs = self.t.adjacent_triangles_to_triangle(&each).unwrap();
+            for adj in &adjs {
+                if self.t.is_finite(adj) == true {
+                    edges.push(cc[0]);
+                    edges.push(cc[1]);
+                    let cc2 = startin::geom::circle_centre(
+                        &pts[adj.v[0]],
+                        &pts[adj.v[1]],
+                        &pts[adj.v[2]],
+                    );
+                    edges.push(cc2[0]);
+                    edges.push(cc2[1]);
+                } else {
+                    let a: usize;
+                    let b: usize;
+                    if adj.v[0] == 0 {
+                        a = adj.v[1];
+                        b = adj.v[2];
+                    } else if adj.v[1] == 0 {
+                        a = adj.v[2];
+                        b = adj.v[0];
+                    } else {
+                        a = adj.v[0];
+                        b = adj.v[1];
+                    }
+                    let mut mid: Vec<f64> = Vec::new();
+                    mid.push((pts[a][0] + pts[b][0]) / 2.0);
+                    mid.push((pts[a][1] + pts[b][1]) / 2.0);
+                    // if outside triangle reverse
+                    let mut v: Vec<f64> = Vec::new();
+                    if startin::geom::orient2d_fast(&pts[b], &pts[a], &cc) >= 0 {
+                        v.push(100. * (mid[0] - cc[0]));
+                        v.push(100. * (mid[1] - cc[1]));
+                    } else {
+                        v.push(100. * (cc[0] - mid[0]));
+                        v.push(100. * (cc[1] - mid[1]));
+                    }
+                    edges.push(cc[0]);
+                    edges.push(cc[1]);
+                    edges.push(cc[0] + v[0]);
+                    edges.push(cc[1] + v[1]);
+                }
+            }
+        }
+        edges
+    }
+
     pub fn closest_point(&self, px: f64, py: f64) -> usize {
         let re = self.t.closest_point(px, py);
-        if re.is_none() {
+        if re.is_err() {
             return 0;
         } else {
             return re.unwrap();
